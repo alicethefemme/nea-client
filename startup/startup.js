32010d2cdce9f89d2d4243447afca92609cdfb65
alicethefemme <alicethefemme@thepenguinclub.xyz>
2025-01-30 10:27:10 +0000

    Finalised changes to add responsive functionality and design to the main pages.

--- startup/startup.js
+++ startup/startup.js
@@ -2,9 +2,12 @@ document.addEventListener('DOMContentLoaded', function() { // Wait for the docum
 
     // Get all the buttons and add their functions.
     document.getElementById("addServer").onclick = function () {addServer()};
+    document.getElementById("add-server-modal-back-button").onclick = function() {addServerBackButton()}
+
     document.getElementById("editServer").onclick = function () {editServer()};
-    document.getElementById("connectToServer").onclick = function () {connectServer()};
     document.getElementById("edit-server-modal-back-button").onclick = function() {editServerBackButton()}
+
+    document.getElementById("connectToServer").onclick = function () {connectServer()};
 
     // Get the select box, and add 10 servers to it.
     const serverSelect = document.getElementById("server")
@@ -15,15 +18,31 @@ document.addEventListener('DOMContentLoaded', function() { // Wait for the docum
         serverSelect.appendChild(opt) // Add the new option to the select menu.
     }
 
-
+    /*
+    Function to open up the "Add Server" modal.
+     */
     function addServer() {
         document.getElementById("add-server-modal").style.display = "block"; // Show the modal
     }
 
+    /*
+    Function to return to home from the "Add Server" modal.
+     */
+    function addServerBackButton() {
+        document.getElementById("add-server-modal").style.display = "none"; // Hide the modal
+        document.getElementById("edit-server-modal-form").reset(); // Reset the information in the form.
+    }
+
+    /*
+    Function to open up the "Edit Server" modal.
+     */
     function editServer() {
         document.getElementById("edit-server-modal").style.display = "block"; // Show the modal
     }
 
+    /*
+    Function to return to home from the "Edit Server" modal.
+     */
     function editServerBackButton() {
         document.getElementById("edit-server-modal").style.display = "none"; // Hide the modal
         document.getElementById("edit-server-modal-form").reset(); // Reset the information in the form.
