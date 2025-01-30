bf66f03775f4b5a6460037914c307224584a8b5e
alicethefemme <alicethefemme@thepenguinclub.xyz>
2025-01-30 09:55:41 +0000

    Added back button functionality

--- startup/startup.js
+++ startup/startup.js
@@ -1,9 +1,10 @@
-document.addEventListener('DOMContentLoaded', function(event) { // Wait for the document to be loaded. Has to be used as the script is referenced in the head of the file.
+document.addEventListener('DOMContentLoaded', function() { // Wait for the document to be loaded. Has to be used as the script is referenced in the head of the file.
 
     // Get all the buttons and add their functions.
     document.getElementById("addServer").onclick = function () {addServer()};
     document.getElementById("editServer").onclick = function () {editServer()};
     document.getElementById("connectToServer").onclick = function () {connectServer()};
+    document.getElementById("edit-server-modal-back-button").onclick = function() {editServerBackButton()}
 
     // Get the select box, and add 10 servers to it.
     const serverSelect = document.getElementById("server")
@@ -21,6 +22,11 @@ document.addEventListener('DOMContentLoaded', function(event) { // Wait for the 
 
     function editServer() {
         document.getElementById("edit-server-modal").style.display = "block"; // Show the modal
+    }
+
+    function editServerBackButton() {
+        document.getElementById("edit-server-modal").style.display = "none"; // Hide the modal
+        document.getElementById("edit-server-modal-form").reset(); // Reset the information in the form.
     }
 
     function connectServer() {
