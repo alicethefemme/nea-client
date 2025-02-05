8c4a71706a189e80c7766ba4f6a99a70386c7cc9
alicethefemme <alicethefemme@thepenguinclub.xyz>
2025-02-05 12:43:45 +0000

    Fixed bug with the new active not removing the hover class, making everything bug out.

--- renderer.js
+++ renderer.js
@@ -4,11 +4,11 @@
  * available in this process because `nodeIntegration` is turned off and
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
+ *
+ * Document for UI elements
  */
 
-// Document to be used for the UI elements.
-
-// Get a mouse over and out script.
+let active = 'sidebar-overview-button';
 
 // Run this every second.
 setInterval(function() {
@@ -17,23 +17,26 @@ setInterval(function() {
 
 
 // Apply a mouseover and mouseout function to all the items in the sidebar, so that they highlight. Change title on active.
-for (let child of document.getElementById('sidebar').children) {
+for (let child of document.getElementById('sidebar').children) { // For all the children in the sidebar.
     child.onmouseover = function () {
-        if (child.className !== 'sidebar-button active') {
-            child.setAttribute('class', `${child.className} hover`);
+        if (!child.className.includes('active')) { // If there is not an active class applied.
+            child.setAttribute('class', `${child.className} hover`); // Add the hover effect.
         }
     };
     child.onmouseout = function () {
-        if (child.className !== 'sidebar-button active') {
-            child.setAttribute('class', child.className.replace('hover', ''))
+        if (!child.className.includes('active')) { // If there is not an active class applied
+            child.setAttribute('class', child.className.replace('hover', '')) // Remove the hover class
         }
     };
     child.onclick = function () {
+
         // Remove the active from the button.
         let prevActive = document.getElementById(active)
-        prevActive.setAttribute('class', prevActive.className.replace('active', ''));
-        if(sidebarReplace(prevActive.id) !== 'settings') {
-            document.getElementById(sidebarReplace(active)).style.display = 'none';
+        prevActive.setAttribute('class', prevActive.className
+            .replace('active', '')
+            .replace('hover', '')); // Get rid of the active and the hover classes.
+        if(sidebarReplace(prevActive.id) !== 'settings') { // If the button isn't settings
+            document.getElementById(sidebarReplace(prevActive.id)).style.display = 'none'; // Hide its menu
         }
 
         // Add the active to the new button.
@@ -42,10 +45,8 @@ for (let child of document.getElementById('sidebar').children) {
 
         // Update main display
         if(sidebarReplace(active) !== "settings") {
-            // Set the content to display.
-            document.getElementById(sidebarReplace(active)).style.display = 'block';
+            document.getElementById(sidebarReplace(active)).style.display = 'block'; // Show the menu.
         }
-
     }
 }
 
