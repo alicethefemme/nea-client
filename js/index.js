ba1a304ede30b39fd9867bce59325eb9970de823
alicethefemme <alicethefemme@thepenguinclub.xyz>
2025-02-05 11:14:51 +0000

    Added changes for more icons, and made the side menu a test functionality.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 
-document.addEventListener('DOMContentLoaded', function() {
+// Document to be used for the main logic of the index file.
+
+document.addEventListener('DOMContentLoaded', function () {
     const title = 'Server Commander';
     const mainTitle = `${title} - Main`;
 
@@ -10,36 +12,47 @@ document.addEventListener('DOMContentLoaded', function() {
     document.title = mainTitle;
 
     // Apply a mouseover and mouseout function to all the items in the sidebar, so that they highlight.
-    for(let child of document.getElementById('sidebar').children) {
-        if (child.id.endsWith('settings-button')) {continue;}
-        child.onmouseover = function() {
+    for (let child of document.getElementById('sidebar').children) {
+        // if (child.id.endsWith('settings-button')) { // Don't change the classes for the settings button.
+        //     continue;
+        // }
+        child.onmouseover = function () {
             if (child.className !== 'sidebar-button active') {
-                child.setAttribute('class', 'sidebar-button hover');
+                child.setAttribute('class', `${child.className} hover`);
             }
         };
-        child.onmouseout = function() {
+        child.onmouseout = function () {
             if (child.className !== 'sidebar-button active') {
-                child.setAttribute('class', 'sidebar-button')
+                child.setAttribute('class', child.className.replace('hover', ''))
             }
         };
-        child.onclick = function() {
+        child.onclick = function () {
             // Remove the active from the button.
-            document.getElementById(active).setAttribute('class', 'sidebar-button');
-            document.getElementById(sidebarReplace(active)).style.display = 'none';
+            let prevActive = document.getElementById(active)
+            prevActive.setAttribute('class', prevActive.className.replace('active', ''));
+            if(sidebarReplace(prevActive.id) !== 'settings') {
+                document.getElementById(sidebarReplace(active)).style.display = 'none';
+            }
 
             // Add the active to the new button.
-            child.setAttribute('class', 'sidebar-button active');
+            child.setAttribute('class', `${child.className} active`);
             active = child.id;
 
-            // Set the content to display.
-            document.getElementById(sidebarReplace(active)).style.display = 'block';
+            // Update main display
+            if(sidebarReplace(active) !== "settings") {
+                // Set the content to display.
+                document.getElementById(sidebarReplace(active)).style.display = 'block';
+            }
+
         }
     }
 })
-/*
-Automatically remove parts of the name to find the child window
-@param {String} name The name of the sidebar button
-@return {String} The name without the sidebar button text
+
+
+/**
+    Automatically remove parts of the name to find the child window
+    @param {String} name The name of the sidebar button
+    @return {String} The name without the sidebar button text
  */
 function sidebarReplace(name) {
     return name.replace('sidebar-', '').replace('-button', '')
--- renderer.js
+++ renderer.js
@@ -5,3 +5,21 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
+
+// Document to be used for the UI elements.
+
+// Get a mouse over and out script.
+
+// Run this every second.
+setInterval(function() {
+    document.getElementById("testtest").innerText = Date.now().toString();
+}, getSeconds(1)) // Time out is in milliseconds.
+
+/**
+    Function to get seconds from milliseconds (* 1000)
+    @param time The time in seconds you want
+    @return The time converted to milliseconds.
+ */
+function getSeconds(time) {
+    return time * 1000
+}
