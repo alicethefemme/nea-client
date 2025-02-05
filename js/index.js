3fa2fca9f03b35c9f336551836fd554348623acf
alicethefemme <alicethefemme@thepenguinclub.xyz>
2025-02-05 11:16:24 +0000

    Moved UI code

--- js/index.js
+++ js/index.js
@@ -10,50 +10,5 @@ document.addEventListener('DOMContentLoaded', function () {
     let active = 'sidebar-overview-button';
 
     document.title = mainTitle;
-
-    // Apply a mouseover and mouseout function to all the items in the sidebar, so that they highlight.
-    for (let child of document.getElementById('sidebar').children) {
-        // if (child.id.endsWith('settings-button')) { // Don't change the classes for the settings button.
-        //     continue;
-        // }
-        child.onmouseover = function () {
-            if (child.className !== 'sidebar-button active') {
-                child.setAttribute('class', `${child.className} hover`);
-            }
-        };
-        child.onmouseout = function () {
-            if (child.className !== 'sidebar-button active') {
-                child.setAttribute('class', child.className.replace('hover', ''))
-            }
-        };
-        child.onclick = function () {
-            // Remove the active from the button.
-            let prevActive = document.getElementById(active)
-            prevActive.setAttribute('class', prevActive.className.replace('active', ''));
-            if(sidebarReplace(prevActive.id) !== 'settings') {
-                document.getElementById(sidebarReplace(active)).style.display = 'none';
-            }
-
-            // Add the active to the new button.
-            child.setAttribute('class', `${child.className} active`);
-            active = child.id;
-
-            // Update main display
-            if(sidebarReplace(active) !== "settings") {
-                // Set the content to display.
-                document.getElementById(sidebarReplace(active)).style.display = 'block';
-            }
-
-        }
-    }
 })
 
-
-/**
-    Automatically remove parts of the name to find the child window
-    @param {String} name The name of the sidebar button
-    @return {String} The name without the sidebar button text
- */
-function sidebarReplace(name) {
-    return name.replace('sidebar-', '').replace('-button', '')
-}
--- renderer.js
+++ renderer.js
@@ -15,6 +15,51 @@ setInterval(function() {
     document.getElementById("testtest").innerText = Date.now().toString();
 }, getSeconds(1)) // Time out is in milliseconds.
 
+
+// Apply a mouseover and mouseout function to all the items in the sidebar, so that they highlight. Change title on active.
+for (let child of document.getElementById('sidebar').children) {
+    child.onmouseover = function () {
+        if (child.className !== 'sidebar-button active') {
+            child.setAttribute('class', `${child.className} hover`);
+        }
+    };
+    child.onmouseout = function () {
+        if (child.className !== 'sidebar-button active') {
+            child.setAttribute('class', child.className.replace('hover', ''))
+        }
+    };
+    child.onclick = function () {
+        // Remove the active from the button.
+        let prevActive = document.getElementById(active)
+        prevActive.setAttribute('class', prevActive.className.replace('active', ''));
+        if(sidebarReplace(prevActive.id) !== 'settings') {
+            document.getElementById(sidebarReplace(active)).style.display = 'none';
+        }
+
+        // Add the active to the new button.
+        child.setAttribute('class', `${child.className} active`);
+        active = child.id;
+
+        // Update main display
+        if(sidebarReplace(active) !== "settings") {
+            // Set the content to display.
+            document.getElementById(sidebarReplace(active)).style.display = 'block';
+        }
+
+    }
+}
+
+
+
+/**
+ Automatically remove parts of the name to find the child window
+ @param {String} name The name of the sidebar button
+ @return {String} The name without the sidebar button text
+ */
+function sidebarReplace(name) {
+    return name.replace('sidebar-', '').replace('-button', '')
+}
+
 /**
     Function to get seconds from milliseconds (* 1000)
     @param time The time in seconds you want
