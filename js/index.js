66481ae4e26f7e1b38f305b1f5972689de7de9db
Alice Joynson-Ellis <alicethefemme@atfdev.co.uk>
2025-02-06 08:53:14 +0000

    Transferred functionality for index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-    window.onresize = function () {
-        if (document.getElementById("right").width <= "400px") {
-            alert("too small!")
-        }
-    }
-});
+/**
+ * Document for use to create the logic for the index page.
+ */
+
+// Run this every second.
+setInterval(function() {
+    document.getElementById("testtest").innerText = Date.now().toString();
+}, getMilliseconds(1)) // Time out is in milliseconds.
+
+/**
+ * A function to return the milliseconds from seconds.
+ * @param time The seconds you want to transform
+ * @returns {number} The milliseconds for the amount of seconds you requested.
+ */
+function getMilliseconds(time) {
+    return time*1000
+}
