e2033515bc4d7e8c0f411b1c58219d0c44152079
Alice Joynson-Ellis <alicethefemme@atfdev.co.uk>
2025-02-07 12:14:03 +0000

    Added changes to the code to make clicking on the main sidebar buttons the only available option. Can then add features to hide and show the submenu buttons.

--- renderer.js
+++ renderer.js
@@ -17,44 +17,49 @@ window.onload = () => {
 
     const titles = new Map();
     titles.set('overview', overviewTitle);
-    titles.set('system-information',systemInformationTitle);
+    titles.set('system-information', systemInformationTitle);
     titles.set('network-information', networkInformationTitle);
     titles.set('docker', dockerTitle);
 
     let active = 'sidebar-overview-button';
     document.title = titles.get(sidebarReplace(active));
 
+    const sidebarButtons = {'sidebar-overview-button': null, 'sidebar-system-information-button': 'sidebar-system-information-submenu', 'sidebar-network-information-button': 'sidebar-network-information-submenu', 'sidebar-docker-button': 'sidebar-docker-submenu'};
+    const extendedSidebarButtons = {...sidebarButtons, 'sidebar-settings-button': null};
+
 // Apply a mouseover and mouseout function to all the items in the sidebar, so that they highlight. Change title on active.
     for (let child of document.getElementById('sidebar').children) { // For all the children in the sidebar.
         child.onmouseover = function () {
-            if (!child.className.includes('active')) { // If there is not an active class applied.
+            if (!child.className.includes('active') && Object.keys(extendedSidebarButtons).includes(child.id)) { // If there is not an active class applied and it belongs to the submenu buttons
                 child.setAttribute('class', `${child.className} hover`); // Add the hover effect.
             }
         };
         child.onmouseout = function () {
-            if (!child.className.includes('active')) { // If there is not an active class applied
+            if (!child.className.includes('active') && Object.keys(extendedSidebarButtons).includes(child.id)) { // If there is not an active class applied and it belongs to the submenu buttons
                 child.setAttribute('class', child.className.replace('hover', '')) // Remove the hover class
             }
         };
         child.onclick = function () {
 
-            // Remove the active from the button.
-            let prevActive = document.getElementById(active)
-            prevActive.setAttribute('class', prevActive.className
-                .replace('active', '')
-                .replace('hover', '')); // Get rid of the active and the hover classes.
-            if(sidebarReplace(prevActive.id) !== 'settings') { // If the button isn't settings
-                document.getElementById(sidebarReplace(prevActive.id)).style.display = 'none'; // Hide its menu
-            }
+            if (Object.keys(sidebarButtons).includes(child.id)) { // Check that it belongs to the submenu buttons.
+                // Remove the active from the button.
+                let prevActive = document.getElementById(active)
+                prevActive.setAttribute('class', prevActive.className
+                    .replace('active', '')
+                    .replace('hover', '')); // Get rid of the active and the hover classes.
+                if (sidebarReplace(prevActive.id) !== 'settings') { // If the button isn't settings
+                    document.getElementById(sidebarReplace(prevActive.id)).style.display = 'none'; // Hide its menu
+                }
 
-            // Add the active to the new button.
-            child.setAttribute('class', `${child.className} active`);
-            active = child.id;
+                // Add the active to the new button.
+                child.setAttribute('class', `${child.className} active`);
+                active = child.id;
 
-            // Update main display
-            if(sidebarReplace(active) !== "settings") {
-                document.getElementById(sidebarReplace(active)).style.display = 'block'; // Show the menu.
-                document.title = titles.get(sidebarReplace(active)); // Set the new title.
+                // Update main display
+                if (sidebarReplace(active) !== "settings") {
+                    document.getElementById(sidebarReplace(active)).style.display = 'block'; // Show the menu.
+                    document.title = titles.get(sidebarReplace(active)); // Set the new title.
+                }
             }
         }
     }
@@ -80,7 +85,6 @@ window.onload = () => {
 }
 
 
-
 /**
  Automatically remove parts of the name to find the child window
  @param {String} name The name of the sidebar button
@@ -91,9 +95,9 @@ function sidebarReplace(name) {
 }
 
 /**
-    Function to get seconds from milliseconds (* 1000)
-    @param time The time in seconds you want
-    @return The time converted to milliseconds.
+ Function to get seconds from milliseconds (* 1000)
+ @param time The time in seconds you want
+ @return The time converted to milliseconds.
  */
 function getSeconds(time) {
     return time * 1000
