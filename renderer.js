3af94f7a7eab4e4376904b28685bdb2f0276e9f8
Alice Joynson-Ellis <alicethefemme@atfdev.co.uk>
2025-02-10 12:57:15 +0000

    Added the hover functionality to the sidebar-sub-buttons. Just implementing them to have a section of the content which they link to.

--- renderer.js
+++ renderer.js
@@ -33,30 +33,28 @@ window.onload = () => {
     };
     const extendedSidebarButtons = {...sidebarButtons, 'sidebar-settings-button': null};
 
-// Apply a mouseover and mouseout function to all the items in the sidebar, so that they highlight. Change title on active.
+    // Apply a mouseover and mouseout function to all the items in the sidebar, so that they highlight. Change title on active.
     for (let child of document.getElementById('sidebar').children) { // For all the children in the sidebar.
         child.onmouseover = function () {
-            if (!child.className.includes('active') && (Object.keys(extendedSidebarButtons).includes(child.id) || Object.values(sidebarButtons).includes(child.id))) { // If there is not an active class applied, and it belongs to the submenu buttons
-                child.setAttribute('class', `${child.className} hover`); // Add the hover effect.
+            if (!child.className.includes('active') && Object.keys(extendedSidebarButtons).includes(child.id)) { // If there is not an active class applied, and it belongs to the submenu buttons
+                child.className = `${child.className} hover`; // Add hover to the classes.
             }
         };
         child.onmouseout = function () {
-            if (!child.className.includes('active') && (Object.keys(extendedSidebarButtons).includes(child.id) || Object.values(sidebarButtons).includes(child.id))) { // If there is not an active class applied, and it belongs to the submenu buttons
+            if (!child.className.includes('active') && Object.keys(extendedSidebarButtons).includes(child.id)) { // If there is not an active class applied, and it belongs to the submenu buttons
                 child.setAttribute('class', child.className.replace('hover', '')) // Remove the hover class
             }
         };
         child.onclick = function () {
-            if (child.id === 'sidebar-settings-button') {
-                window.electron.send('load-settings'); // Load the settings window.
+            if (child.id === 'sidebar-settings-button') { // Load the settings window on button press.
+                window.electron.send('load-settings');
             }
             if (Object.keys(sidebarButtons).includes(child.id)) { // Check that it belongs to the submenu buttons.
 
                 // Remove the active from the button.
                 let prevActive = document.getElementById(active);
-                prevActive.setAttribute('class', prevActive.className
-                    .replace('active', '')
-                    .replace('hover', '')); // Get rid of the active and the hover classes.
-                document.getElementById(sidebarReplace(prevActive.id)).style.display = 'none'; // Hide its menu
+                prevActive.className = prevActive.className.replace('active', '').replace('hover', ''); // Remove the hover and active classes.
+                document.getElementById(sidebarReplace(prevActive.id)).style.display = 'none'; // Hide the content connected to it.
 
                 // Get the submenu buttons for the previously active selection and hide them.
                 if (sidebarButtons[prevActive.id] !== null) {
@@ -64,20 +62,31 @@ window.onload = () => {
                 }
 
                 // Add the active to the new button.
-                child.setAttribute('class', `${child.className} active`);
+                child.className = `${child.className} active`;
                 active = child.id;
-
-                // Update main display
-                document.getElementById(sidebarReplace(active)).style.display = 'block'; // Show the menu.
-                document.title = titles[sidebarReplace(active)]; // Set the new title.
 
                 // Show the new submenu buttons.
                 if (sidebarButtons[active] !== null) {
                     document.getElementById(sidebarButtons[active]).style.display = 'block';
                 }
+                // Update main display
+                document.title = titles[sidebarReplace(active)]; // Set the new title.
             }
         }
     }
+
+    // Apply a mouseover, mouseout and click function to all the items in the sidebar submenus
+    for (let submenuContainer of document.getElementsByClassName('sidebar-sub-button')) {
+        for (let child of submenuContainer.children) {
+            child.onmouseover = function () {
+                child.className = `${child.className} hover`
+            };
+            child.onmouseout = function () {
+                child.className = child.className.replace('hover', '')
+            };
+        }
+    }
+
 
     const myChart = new Chart(document.getElementById('overview-cpu').getContext('2d'), { // Create a new chart and provide it the 2D context of the chart object in HTML.
         type: 'line',
