90322167f0ee73dded48e6e53fefdd03026c4c2e
Alice Joynson-Ellis <alicethefemme@atfdev.co.uk>
2025-02-12 14:59:42 +0000

    Added the ability to get the settings file.

--- js/classes/settings.js
+++ js/classes/settings.js
@@ -2,13 +2,19 @@ class Settings {
     constructor(options = {}) {
         // Fill out all the defaults, and then allow them to be overridden.
         Object.assign(this, {
-            reloadTime: 15
+            reloadTime: 15,
+            customLogPath: ''
         }, options)
     }
 
     getValues() {
         return {
-            reloadTime: this.reloadTime
-        }
+            reloadTime: this.reloadTime,
+            customLogPath: this.customLogPath
+        };
     }
+}
+
+module.exports = {
+    Settings
 }
--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, ipcMain } = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
 const path = require('node:path')
 const fs = require('node:fs')
-const Settings = require('./js/classes/settings.js')
+const {Settings} = require('./js/classes/settings')
 
 /**
  * Checks if the settings file has all the right headers.
@@ -10,106 +10,111 @@ const Settings = require('./js/classes/settings.js')
  * @returns {boolean} The validity of this content
  */
 function checkValiditySettings(settingData) {
-  if(settingData){
+    let settingsInfo = {
+        'reloadTime': Number, 'customLogPath': String
+    }
 
-  }
+    for (let header of Object.keys(settingsInfo)) {
+        if (!Object.keys(settingData).includes(header)) {
+            return false;
+        }
+        if (settingData[header].type === settingsInfo[header]) {
+            return false;
+        }
+    }
 
-  return false;
+    return true;
 }
 
 
+/**
+ * Gets the setting file from the user folder and reads the contents.
+ */
 function getSettings() {
-  const appData = app.getPath('userData');
+    const appData = app.getPath('userData');
 
-  const settingFile = path.join(appData, 'settings.json');
-  let settings = new Settings();
+    const settingFile = path.join(appData, 'settings.json');
+    let settings = new Settings();
 
-  fs.readFile(settingFile, 'utf-8', (err, data) => {
-    if(err) {
-      fs.writeFile(settingFile, JSON.stringify(settings.getValues()), (err) => {
-        console.error(err);
-      });
-      console.log('Generated default values for file which does not exist.')
-    }
-    const fileData = JSON.parse(data);
+    fs.readFile(settingFile, 'utf-8', (err, data) => {
+        if (err) {
+            fs.writeFile(settingFile, JSON.stringify(settings.getValues()), (err) => {
+                console.error(err)
+            });
+            console.log('Generated default values for file which does not exist.')
+        }
+        const fileData = JSON.parse(data);
 
-    if(!checkValiditySettings(fileData)) {
-      fs.writeFile(settingFile, JSON.stringify(settings.getValues()), (err) => {
-        console.error(err);
-      });
-      console.log('Generated default values for file which doesn\'t have correct or valid values');
-    }
+        // Check if the settings are valid and if not set the file to the default valid settings.
+        if (!checkValiditySettings(fileData)) {
+            fs.writeFile(settingFile, JSON.stringify(settings.getValues()), (err) => {
+                console.error(err)
+            });
+            console.log('Generated default values for file which does not have correct or valid values');
+        }
 
-    console.log('Finished settings!')
-  })
+        return fileData;
+
+    })
 
 }
 
-function createStartupWindow () {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    resizable: false,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      contextIsolation: true
-    }
-  })
+function createStartupWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800, height: 600, resizable: false, webPreferences: {
+            preload: path.join(__dirname, 'preload.js'), contextIsolation: true
+        }
+    })
 
-  mainWindow.loadFile('startup.html')
+    mainWindow.loadFile('startup.html')
 
-  // Load the main application window
-  ipcMain.on('load-main', () => {
-    mainWindow.setMinimumSize(1280, 720);
-    mainWindow.setSize(1280, 720);
-    mainWindow.resizable = true;
+    // Load the main application window
+    ipcMain.on('load-main', () => {
+        mainWindow.setMinimumSize(1280, 720);
+        mainWindow.setSize(1280, 720);
+        mainWindow.resizable = true;
 
-    mainWindow.loadFile("index.html");
-  });
-
-  ipcMain.on('load-settings', () => {
-    let settingsWindow = new BrowserWindow({
-      parent: mainWindow,
-      modal: true,
-      width: 800,
-      height: 600,
-      resizable: false
+        mainWindow.loadFile("index.html");
     });
 
-    settingsWindow.loadFile('settings.html');
-  });
+    ipcMain.on('load-settings', () => {
+        let settingsWindow = new BrowserWindow({
+            parent: mainWindow, modal: true, width: 800, height: 600, resizable: false
+        });
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+        settingsWindow.loadFile('settings.html');
+    });
+
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
 }
 
 ipcMain.handle('get-data', (dataType) => {
-  switch (dataType) {
-    case 'settings': getSettings()
-  }
+    switch (dataType) {
+        case 'settings':
+            return getSettings();
+    }
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createStartupWindow()
+    createStartupWindow()
 
-  getSettings()
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createStartupWindow()
-  })
+    app.on('activate', function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createStartupWindow()
+    })
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  // if (process.platform !== 'darwin') app.quit()
-  app.quit() // Overridden convention for development purposes.
+    // if (process.platform !== 'darwin') app.quit()
+    app.quit() // Overridden convention for development purposes.
 })
 
 // In this file you can include the rest of your app's specific main process
