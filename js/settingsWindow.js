85594d7fcfbec05b65769373ea6e6e2301e0ba46
Alice Joynson-Ellis <alicethefemme@atfdev.co.uk>
2025-02-16 21:56:36 +0000

    Added some basic inputs. Testing

--- js/settingsWindow.js
+++ js/settingsWindow.js
@@ -1,16 +1,22 @@
 
-let testParagraph = document.getElementById('test');
+let reloadTimeInput = document.getElementById('reload-time-input');
 
 document.addEventListener('DOMContentLoaded', () => {
     window.electron.invoke('get:data', 'settings').then((settings) => {
-        testParagraph.innerText = settings.reloadTime // Ensure that you get settings with their given string title.
+        reloadTimeInput.value = settings.reloadTime
     })
     // console.log(JSON.parse(settings));
 
 });
 
+/**
+ * EVENT LISTENERS
+ */
 document.addEventListener('keyup', (key) => {
     if (key.key === 'Escape') {
         window.electron.send('close:settings');
     }
+});
+document.getElementById('custom-log-path-select-folder-button').addEventListener('click', () => {
+    console.log('test');
 })
