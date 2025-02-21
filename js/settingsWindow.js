683c8a7f236481f58a0559d93d01c8c531239948
Alice <--replace-all>
2025-02-21 20:55:13 +0000

    Made basic changes to format the settings window.

--- js/settingsWindow.js
+++ js/settingsWindow.js
@@ -1,17 +1,14 @@
 
 let reloadTimeInput = document.getElementById('reload-time-input');
 
-document.addEventListener('DOMContentLoaded', () => {
-    window.electron.invoke('get:data', 'settings').then((settings) => {
-        reloadTimeInput.value = settings.reloadTime
-    })
-    // console.log(JSON.parse(settings));
-
-});
+window.electron.invoke('get:data', 'settings').then((settings) => {
+    reloadTimeInput.value = settings.reloadTime
+})
 
 /**
  * EVENT LISTENERS
  */
+
 document.addEventListener('keyup', (key) => {
     if (key.key === 'Escape') {
         window.electron.send('close:settings');
@@ -20,3 +17,26 @@ document.addEventListener('keyup', (key) => {
 document.getElementById('custom-log-path-select-folder-button').addEventListener('click', () => {
     console.log('test');
 })
+
+/**
+ * RENDERING THE PAGE
+ */
+
+window.onload = () => {
+    const sidebarButtons = document.getElementsByClassName('sidebar-button');
+
+    for (let sidebarBttn of sidebarButtons) {
+        sidebarBttn.onmouseover = function() {
+            if(!sidebarBttn.className.includes('active')) {
+                sidebarBttn.className = `${sidebarBttn.className} hover`
+            }
+        };
+        sidebarBttn.onmouseout = function() {
+            if(!sidebarBttn.className.includes('active')) {
+                sidebarBttn.className = sidebarBttn.className.replace(' hover', '');
+            }
+        }
+    }
+}
+
+
